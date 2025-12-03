4be980a9e6939f92e7eb53982a452fe7d9422103
LAPTOP-21O86I06\LZM <1104407395@qq.com>
2025-12-03 14:35:28 +0800

    试验Scoket效果

---
+++ Client/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 连接到服务器的 IP 地址和端口
+            string serverIP = "127.0.0.1"; // 本机地址
+            int port = 8080;
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(serverIP), port);
+
+            // 创建 TCP Socket
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // 连接服务器
+            clientSocket.Connect(endPoint);
+            Console.WriteLine("已连接到服务器.");
+
+            // 发送数据
+            string message = "Hello from client!";
+            clientSocket.Send(Encoding.UTF8.GetBytes(message));
+
+            // 接收服务器回应
+            byte[] buffer = new byte[1024];
+            int bytesReceived = clientSocket.Receive(buffer);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            Console.WriteLine("收到服务器回应: " + response);
+
+            // 关闭连接
+            clientSocket.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WPF_ChatServer/WPF_ChatServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ChatServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 设置监听的 IP 地址和端口号
+            int port = 8080;
+            IPAddress ipAddress = IPAddress.Any;
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+
+            // 创建 TCP Socket
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // 绑定端口并开始监听
+            listener.Bind(endPoint);
+            listener.Listen(10);
+            Console.WriteLine("服务器已启动，等待连接...");
+
+            // 等待并接收客户端连接
+            Socket clientSocket = listener.Accept();
+            Console.WriteLine("客户端已连接.");
+
+            // 接收客户端数据
+            byte[] buffer = new byte[1024];
+            int bytesReceived = clientSocket.Receive(buffer);
+            string data = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+            Console.WriteLine("收到数据: " + data);
+
+            // 向客户端发送回应
+            string response = "Hello from server!";
+            clientSocket.Send(Encoding.UTF8.GetBytes(response));
+
+            // 关闭连接
+            clientSocket.Close();
+            listener.Close();
+            Console.ReadLine();
+        }
+    }
+}
